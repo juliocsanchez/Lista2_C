745fbb4109b96d697e2d86611a604d661f86161f
juliocsanchez <juliocspfc@gmail.com>
2024-03-19 00:39:54 -0300

    troca de questão

--- q28.c
+++ q28.c
@@ -2,13 +2,6 @@
 
 int main()
 {
-    unsigned long long int res = 0;
-    unsigned long long int multiplo = 1;
-    for (int i = 0; i < 64; i++)
-    {
-        res += multiplo;
-        multiplo *= 2;
-    }
-    printf("Total de grãos: %llu\n", res);
+
     return 0;
 }
